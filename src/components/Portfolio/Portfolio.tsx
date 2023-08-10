4b18017dfb113e39bed116eb0287d32f15eea7bd
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2023-08-10 20:17:23 +0100

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -75,7 +75,7 @@ export function Portfolio(){
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="#66bfff" stroke="#66bfff " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://github.com/CodeWunder/Planets-fact" target="_blank" rel="noreferrer">
+              <a href="https://github.com/CodeWunder/Interactive-Comment-App" target="_blank" rel="noreferrer">
                 <img src={githubIcon} alt="GitHub" />
               </a>
               <a href="https://codewunder-planet.netlify.app/" target="_blank" rel="noreferrer">
