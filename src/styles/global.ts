28f5825703b8a148e037331b171181d8f1828759
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2023-08-11 11:43:18 +0100

    Update global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -76,10 +76,9 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
   }
-  *:focus{
-  outline: none !important;
-  }
+  
 
   html{
     font-size: 62.5%;
