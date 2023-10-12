e3b79f375066c7d626d2c986b46a9d1052fd1004
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2023-10-12 15:26:19 +0100

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -136,21 +136,24 @@ export function Portfolio(){
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="#66bfff" stroke="#66bfff " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://codewunder.vercel.app/" target="_blank" rel="noreferrer">
+              <a href="https://github.com/CodeWunder/Web-Dictionary-App" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="GitHub" />
+              </a>
+              <a href="https://web-dictionary-app-one.vercel.app/" target="_blank" rel="noreferrer">
                 <img src={externalLinkIcon} alt="Visit Site" />
               </a>
             </div>
             </header>
             <div className="body">
-              <h3>Personal Portfolio</h3>
-              <p>A blend of TypeScript and React. Experience seamless scrolling and captivating animations. Let's connect through my projects and journey!         
+              <h3>Dictionary App</h3>
+              <p>I integrated with the Dictionary API to create a real-world dictionary web app and also you could use it in your preferred colour mode.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li><img src={reactIcon} alt="React Icon" /></li>
-                <li><img src={typescriptIcon} alt="Typescript Icon" /></li>
-                <li><img src={cssIcon} alt="CSS Icon" /></li>                
+                <li><img src={reactIcon} alt="React" /></li>
+                <li><img src={javascriptIcon} alt="JavaScript" /></li>
+                <li><img src={sassIcon} alt="SCSS" /></li>                
             </ul>
             </footer>
           </div>
